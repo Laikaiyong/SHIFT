f1fc73b990f390c94e08367cee56eadb53cc3270
Vandyck <vandycklai@gmail.com>
2024-10-06 04:47:08 +0800

    Hotfix again evil

--- frontend/src/app/api/requests.js
+++ frontend/src/app/api/requests.js
@@ -4,7 +4,7 @@ import PromptContract from "../../abi/Prompt.json";
 const contractAddress = "0x9e8550203C66A2665b007a1f6Bcff1b25E148403"; // Replace with your contract address
 
 // Initialize ethers provider and contract
-const signer = ethereum.selectedAddress;
+const signer = typeof window !== 'undefined' ? window.ethereum.selectedAddress : null;
 const aiOracleContract = new ethers.Contract(contractAddress, PromptContract, signer);
 
 export const getRequest = async (requestId) => {
